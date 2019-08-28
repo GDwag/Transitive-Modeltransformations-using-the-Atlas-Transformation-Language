391b981ecb0d74e2bb8f165865b5b41f1dc03282
Gilbert Groten <gilbert.groten@orange.fr>
2019-08-28 23:06:33 +0200

    created EMFComparePluginTest - still not working, and still having a main method, because i wanted to first figure out if it works, then if i can run it as a plugin

---
+++ EMFComparPluginTest/src/atlTransformationEMFCompare/Main.java
@@ -0,0 +1,9 @@
+package atlTransformationEMFCompare;
+
+public class Main {
+	public static void main(String[] args) {
+		CompareTest compareTest = new CompareTest();
+		compareTest.setUp();
+		compareTest.run();
+	}
+}
