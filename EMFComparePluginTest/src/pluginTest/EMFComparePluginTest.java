1455a81e3ffb988214bcdfdcafba8c844f726e89
Gilbert Groten <gilbert.groten@orange.fr>
2019-09-21 16:07:43 +0200

    cleaned up even more stuff and added license

--- EMFComparePluginTest/src/pluginTest/EMFComparePluginTest.java
+++ EMFComparePluginTest/src/pluginTest/EMFComparePluginTest.java
@@ -1,41 +1,39 @@
+/*
+ * Contributors:
+ * Gilbert Groten - gilbert@groten.fr
+ * 
+ * This program is made available under the terms of the Eclipse Public License - v 1.0
+ */
+
 package pluginTest;
 
 import org.eclipse.uml2.uml.resource.UMLResource;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 import org.eclipse.emf.compare.Diff;
 //import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.compare.Comparison;
 import org.eclipse.emf.compare.EMFCompare;
-//import org.eclipse.emf.compare.merge.BatchMerger;
-//import org.eclipse.emf.compare.merge.IBatchMerger;
-//import org.eclipse.emf.compare.merge.IMerger;
 import org.eclipse.emf.compare.scope.DefaultComparisonScope;
 import org.eclipse.emf.compare.scope.FilterComparisonScope;
-import org.eclipse.emf.compare.scope.IComparisonScope;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.uml2.uml.internal.resource.UMLResourceFactoryImpl;
-import org.eclipse.emf.compare.utils.EMFComparePredicates;
 
 public class EMFComparePluginTest {
 	
 	@Test
 	public void compareUML_XMI() {
-		//assert compare("../MediaStoreUML/MediaStoreUML.uml", "../Palladio2UML/Model/ms_small.uml");
 		assert compare("../Palladio2UML/Model/MediaStoreUML_copy_2019_09_18", "../Palladio2UML/Model/ms_small_copy_2019_09_18", false);
 	}
 	
 	@Test
 	public void compareUML_UML() {
-		//assert compare("../MediaStoreUML/MediaStoreUML.uml", "../Palladio2UML/Model/ms_small.uml");
 		assert compare("../MediaStoreUML/MediaStoreUML.uml", "../Palladio2UML/Model/ms_small.uml", true);
 	}
 	
@@ -57,41 +55,15 @@ public class EMFComparePluginTest {
 	    resourceSet1.getResource(uri1, true);
 	    resourceSet2.getResource(uri2, true);
 
-	    FilterComparisonScope scope = new DefaultComparisonScope(resourceSet1, resourceSet2, null); //FilterComparisonScope(resourceSet1, resourceSet2, null);
-	    
-	    //sollte helfen diffs, die keinen inhaltlihcen konflikt darstellen loszuwerden
-	    //https://www.programcreek.com/java-api-examples/?api=org.eclipse.emf.compare.scope.DefaultComparisonScope
-	    //https://help.eclipse.org/oxygen/index.jsp?topic=%2Forg.eclipse.emf.compare.doc%2Fhelp%2Fdeveloper%2Fjavadoc%2Forg%2Feclipse%2Femf%2Fcompare%2Futils%2FEMFComparePredicates.html&anchor=CONTAINMENT_REFERENCE_CHANGE
-		//scope.setResourceContentFilter(EMFComparePredicates.WITHOUT_CONFLICT);
-		//scope.setEObjectContentFilter(EMFComparePredicates.WITHOUT_CONFLICT);
-		
+	    FilterComparisonScope scope = new DefaultComparisonScope(resourceSet1, resourceSet2, null);
 	    Comparison comparison = EMFCompare.builder().build().compare(scope);
 
 	    List<Diff> differences = comparison.getDifferences();
 	    if (! differences.isEmpty()) {
-	    	/*
-	    	System.out.println();
-	    	System.out.println("----- IGNORED DIFFERENCES -----");
-	    	//for (int i = 0; i < differences.size(); i++) {
-	    		//Diff difference = differences.get(i);
-	    	for (Diff difference : differences) {
-	    		int i = differences.indexOf(difference);
-	    		if (difference.getConflict() == null) { //TODO WARNING - good condition?
-	    			differences.remove(i);
-	    			System.out.println("Difference Ignored (because no conflict): " + difference.toString());
-	    		}
-	    		//trying to get rid of differences that don't matter to us
-	    	}
-	    	*/
 	    	System.out.println();
 	    	System.out.println("----- CONTENT OF DIFFERENCES -----");
 	    	for (Diff difference : differences) System.out.println(difference.toString());
-	    	//for (Diff difference : differences) System.out.println(difference.getConflict());//.getKind());//getKind().getName());
 	    }
 	    return differences.isEmpty();
-	    // Let's NOT merge every single diff, because that's not the point - at least not in the file ("java.lang.RuntimeException: The resource already exists at that location.")
-	    //IMerger.Registry mergerRegistry = IMerger.RegistryImpl.createStandaloneInstance();// "new IMerger.RegistryImpl();" disencouraged by https://www.eclipse.org/forums/index.php/t/531137/
-	    //IBatchMerger merger = new BatchMerger(mergerRegistry);
-	    //merger.copyAllLeftToRight(differences, new BasicMonitor());
 	}
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+This workspace contains example ATL-Transformatios from The Palladio Component Model to UML and JaMoPP, and from UML to JaMoPP, and an EMFCompare-Plugin-Test for the resulting Models.
+All source Code in this Project has been contributed by Gilbert Groten, as well as the "MediaStoreUML" Model, and all his contributions are made available under the terms of the Eclipse Public License - v 1.0
+TODO
+	The "MediaStoreSmallerModel" has been contributed by the 'Chair for Software Design and Quality' of the 'Karlsruhe Institute of Technology" under the terms of ???
+TODO
